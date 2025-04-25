5f3bcc2b0c4e767a897b8811d3e3c45b0b67bce9
sujin <ryusujin82@naver.com>
2025-04-25 16:21:15 +0900

    변수에 대하여~

--- 240425/first.py
+++ 240425/first.py
@@ -4,12 +4,35 @@
     여러줄 주석
 '''
 
-print("오징어와 말미잘")
-print("말미잘과 오징어")
-print("★  ★  ★  ★  ★  ★  ★")
-print("★                 ★")
-print("★                 ★")
-print("★                 ★")
-print("★                 ★")
-print("★                 ★")
-print("★  ★  ★  ★  ★  ★  ★")
+# print("오징어와 말미잘")
+# print("말미잘과 오징어")
+# print("★  ★  ★  ★  ★  ★  ★")
+# print("★                 ★")
+# print("★                 ★")
+# print("★                 ★")
+# print("★                 ★")
+# print("★                 ★")
+# print("★  ★  ★  ★  ★  ★  ★")
+
+# 변수
+food = "김치찌개"
+food = "김치 볶음밥" 
+
+
+food2 = "김치찌개"
+print(food2)
+food2 = "김치 볶음밥" 
+print(food2)
+
+
+name = "sujin"
+address = "경기도 부천"
+
+# 10 + 10 = 20
+number01 = 10
+number02 = 10
+result = number01 + number02
+print(number01, "+", number02, "=", result)
+
+# 상수 : 절대값(대문자 작성)
+PI = 3.14
