ebf7618d615b41c7c31e13950faea29e4d971559
sujin <ryusujin82@naver.com>
2025-04-29 12:21:19 +0900

    250429 start

---
+++ 250429/first.py
@@ -0,0 +1,17 @@
+# 비교연산
+x, y = 10, 100
+print(x == y)
+
+x, y = 10, 10
+# print(x > y) # 초과
+# print(x >= y) # 이상
+# print(x < y) # 미만
+# print(x <= y) # 이하
+
+# print('apple' < 'banana')
+
+txt1 = input()
+txt2 = input()
+a, b = len(txt1), len(txt2)
+print(f"첫번째 변수의 문자 수는 {a} 입니다")
+print(f"두번째 변수의 문자 수는 {b} 입니다")
